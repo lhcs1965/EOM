879adb27384071ed30d13137daa721c92c455ce2
lhcs1965 <lhcs1965@gmail.com>
2025-01-04 00:55:30 -0300

    bootstrap on line

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
-<script src='js/jquery-3.7.1.js'></script>
-<script src='js/bootstrap/bootstrap.bundle.min.js'></script>
-<script src='js/datatables/dataTables.js'></script>
-<script src="js/datatables/dataTables.bootstrap5.js"></script>
-<script src="js/datatables/moment.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.7.1.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
+<script src="https://cdn.datatables.net/2.1.8/js/dataTables.bootstrap5.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
 <!-- <script src="js/datatables/dateTime.js"></script> -->
 
--- includes/header.php
+++ includes/header.php
@@ -2,8 +2,7 @@
     <meta charset='utf-8'>
     <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
     <title>FC - <?=$menu?></title>
-    <link rel='stylesheet' href='css/bootstrap/bootstrap.min.css'>
-    <!-- <link rel='stylesheet' href='css/datatables/dataTables.dataTables.css'> -->
-    <link rel='stylesheet' href='css/datatables/dataTables.bootstrap5.css'>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/css/bootstrap.min.css">
+    <link rel='stylesheet' href='https://cdn.datatables.net/2.1.8/css/dataTables.bootstrap5.css'>
     <link rel='stylesheet' href='css/custom.css'>
 </head>
