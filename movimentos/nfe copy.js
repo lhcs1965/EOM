6399e0248ecfbd63374d53fa8759f63c1b90c88b
lhcs1965 <lhcs1965@gmail.com>
2025-01-11 23:31:49 -0300

    nfe2

--- movimentos/nfe copy.js
+++ movimentos/nfe copy.js
@@ -6,36 +6,29 @@ $(function () {
                 var reader = new FileReader();
                 reader.onload = function (e) {
                     var xmlDoc = $.parseXML(e.target.result);
-                    var emit = $(xmlDoc).find("emit");
-                    //Add the data rows.
-                    $(emit).each(function () {
-                        $(this).children().each(function (e) {
-                            $($("input:text")[e]).val($(this).text())
-                        })
-                    })
-
-                    var dvTable = $("#dvTable");
-                    dvTable.html("");
-                    dvTable.append(table);
-                    var cobr = $(xmlDoc).find("cobr");
+                    var nfe = $(xmlDoc).find("emit");
+                    //var nfe = $(xmlDoc).find("cobr");
 
                     //Create a HTML Table element.
                     var table = $("<table />");
                     table[0].border = "1";
+
                     //Add the header row.
-                    // cobr.eq(0).children().each(function () {
-                    //     var row = $(table[0].insertRow(-1));
-                    //     var headerCell = $("<th />");
-                    //     headerCell.html(this.nodeName);
-                    //     row.append(headerCell);
-                    // });
+                    var row = $(table[0].insertRow(-1));
+                    nfe.eq(0).children().each(function () {
+                        var headerCell = $("<th />");
+                        headerCell.html(this.nodeName);
+                        row.append(headerCell);
+                    });
+
                     //Add the data rows.
-                    $(cobr).each(function () {
-                        $(this).children().each(function (e) {
-                            row = $(table[0].insertRow(-1));
+                    $(nfe).each(function () {
+                        row = $(table[0].insertRow(-1));
+                        $(this).children().each(function (e) { // <-- adicione 'e' como parâmetro para pegar os índices do laço
                             var cell = $("<td />");
                             cell.html($(this).text());
                             row.append(cell);
+                        //$($("input:text")[e]).val($(this).text()); // <-- insere valores nos campos
                         });
                     });
   
